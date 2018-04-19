bea5303ea42ea3687f4c7e7cf0135cbde9bb32c7
josephcf <ch.fr.joseph@gmail.com>
2018-04-19 16:08:11 +0530

    Rename index.js to index_fix.js

---
+++ index_fix.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var has = Object.prototype.hasOwnProperty;
+
+/**
+ * Decode a URI encoded string.
+ *
+ * @param {String} input The URI encoded string.
+ * @returns {String} The decoded string.
+ * @api private
+ */
+function decode(input) {
+  return decodeURIComponent(input.replace(/\+/g, ' '));
+}
+
+/**
+ * Simple query string parser.
+ *
+ * @param {String} query The query string that needs to be parsed.
+ * @returns {Object}
+ * @api public
+ */
+function querystring(query) {
+  var parser = /([^=?&]+)=?([^&]*)/g
+    , result = {}
+    , part;
+
+  //
+  // Little nifty parsing hack, leverage the fact that RegExp.exec increments
+  // the lastIndex property so we can continue executing this loop until we've
+  // parsed all results.
+  //
+  for (;
+    part = parser.exec(query);
+    var tk1, tk2;
+    tk1 = decode(part[1])
+    tk2 = decode(part[2])
+    //checking if we already have the key in result object && it is not an array
+    if( has.call(result, tk1) && !_.isArray(result[tk1]) ) {
+        var temp = result[tk1]; 
+        result[tk1] = [];
+        result[tk1].push(temp)
+        result[tk1].push(tk2)
+    } else if(_.isArray(result[tk1])){ //if the key is an array, push the new value
+        result[tk1].push(tk2)
+    } else { //assign value
+        result[tk1] = tk2;
+    }
+  );
+
+  return result;
+}
+
+/**
+ * Transform a query string to an object.
+ *
+ * @param {Object} obj Object that should be transformed.
+ * @param {String} prefix Optional prefix.
+ * @returns {String}
+ * @api public
+ */
+function querystringify(obj, prefix) {
+  prefix = prefix || '';
+
+  var pairs = [];
+
+  //
+  // Optionally prefix with a '?' if needed
+  //
+  if ('string' !== typeof prefix) prefix = '?';
+
+  for (var key in obj) {
+    if (has.call(obj, key)) {
+      pairs.push(encodeURIComponent(key) +'='+ encodeURIComponent(obj[key]));
+    }
+  }
+
+  return pairs.length ? prefix + pairs.join('&') : '';
+}
+
+//
+// Expose the module.
+//
+exports.stringify = querystringify;
+exports.parse = querystring;
